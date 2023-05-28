0ba24c6dcc5fe3c92fb73c5a2d60d67515d36550
akiraxcv <valera.petuhov.89@gmail.com>
2023-05-29 01:29:01 +0300

    task2
    t2

---
+++ 1day1task/1day1task/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _1day1task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                int n = 230; // диапазон поиска простых чисел
+                bool[] primes = new bool[n + 1];
+
+                for (int i = 2; i <= n; i++)
+                {
+                    primes[i] = true;
+                }
+
+                for (int i = 2; i * i <= n; i++)
+                {
+                    if (primes[i])
+                    {
+                        for (int j = i * i; j <= n; j += i)
+                        {
+                            primes[j] = false;
+                        }
+                    }
+                }
+
+                Console.WriteLine("Простые числа от 1 до {0}: ", n);
+                for (int i = 2; i <= n; i++)
+                {
+                    if (primes[i])
+                    {
+                        Console.Write("{0} ", i);
+                    }
+                }
+            }
+        }
+    }
---
+++ 1day2task/1day2task/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _1day2task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку текста: ");
+            string input = Console.ReadLine();
+
+            // Разделяем строку на массив слов
+            string[] words = input.Split(new char[] { ' ', ',', '.', ':', ';', '-', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+
+            Console.WriteLine("Слова, начинающиеся на букву 'а':");
+            foreach (string word in words)
+                if (word.StartsWith("а", StringComparison.OrdinalIgnoreCase))
+                    Console.WriteLine(word);
+
+            Console.ReadKey();
+        }
+    }
+}
