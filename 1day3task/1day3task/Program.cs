2ae4b74feb3b76542eb789eab7dc183a51c5a3d1
akiraxcv <valera.petuhov.89@gmail.com>
2023-05-29 01:31:59 +0300

    task3
    t3

---
+++ 1day3task/1day3task/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _1day3task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число N: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int a = 0;
+            int b = 1;
+            int c;
+
+            Console.WriteLine($"Первые {n} чисел Фибоначчи:");
+            Console.Write($"{a}, {b}");
+
+            for (int i = 2; i < n; i++)
+            {
+                c = a + b;
+                Console.Write($", {c}");
+                a = b;
+                b = c;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
